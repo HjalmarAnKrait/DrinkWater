f65abb554f275b49d4790ee30275a875af82d7a3
Egor Tarabrin <tarabrin2142@gmail.com>
2020-04-12 22:18:23 +0700

    Initial commit

---
+++ app/src/main/java/com/example/drinkwater/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.example.drinkwater;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
